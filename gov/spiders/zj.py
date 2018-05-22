4fc70edc85683a4473fe2afd9449cd90d069059e
arshesoftime <Richardwuyue@gmail.com>
2018-05-22 17:00:17 +0800

    Update zj.py

--- gov/spiders/zj.py
+++ gov/spiders/zj.py
@@ -12,7 +12,7 @@ class zjSpider(Spider):
     name = 'zj'
     page = 500
     count = 1
-    def __init__(self):
+    def __init__(self,**kwargs):
         self.browser = webdriver.Chrome(chrome_options=chrome_options)
         self.browser.set_page_load_timeout(30)
         self.browser2=webdriver.Chrome(chrome_options=chrome_options)
