fa80e07fdc0de069119a3bea543fa3b751638a51
arshesoftime <Richardwuyue@gmail.com>
2018-05-22 16:59:43 +0800

    Update sh.py

--- gov/spiders/sh.py
+++ gov/spiders/sh.py
@@ -14,7 +14,7 @@ class shSpider(Spider):
     name = 'sh'
     page = 500
     count = 1
-    def __init__(self):
+    def __init__(self,**kwargs):
         self.browser = webdriver.Chrome(chrome_options=chrome_options)
         self.browser.set_page_load_timeout(30)
         self.browser2=webdriver.Chrome(chrome_options=chrome_options)
